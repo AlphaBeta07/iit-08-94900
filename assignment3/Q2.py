7947355e8837acb4861a930d47af05ee7fb0f004
AlphaBeta07 <anishlandage007@gmail.com>
2025-12-15 10:15:28 +0530

    first commit

--- assignment3/Q2.py
+++ assignment3/Q2.py
@@ -6,6 +6,10 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 def log_in():
     st.title("Login Weather App")
+    if "Logout_message" in st.session_state:
+        st.success(st.session_state["Logout_message"])
+        del st.session_state["Logout_message"]
+
     username = st.text_input("Enter Username")
     password = st.text_input("Enter password", type="password")
     if st.button("login", type = "primary"):
@@ -20,6 +24,6 @@ def log_in():
 def log_out():
     logout = st.button("Logout", type = "primary")
     if logout:
+        st.session_state["Logout_message"] = "Thanks, You have been logged out Sucessfully"
         st.session_state["Login"] = False
-        st.info("thanks")
         st.rerun() 
