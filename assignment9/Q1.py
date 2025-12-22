ea3cb12196d7e4d81dc75e315c93cb8bb70e00e7
AlphaBeta07 <anishlandage007@gmail.com>
2025-12-22 11:41:19 +0530

    updated

--- assignment9/Q1.py
+++ assignment9/Q1.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-import time
 
 load_dotenv()
 
